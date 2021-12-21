820281a746c92dc793c0e0b98f7770ce5f5f49ff
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-12-20 23:22:40 -0300

    Implementando mapeamento das classes com o automapper

---
+++ src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
+using Api.Domain.Models;
+using AutoMapper;
+
+namespace Api.CrossCutting.Mappings
+{
+    public class DtoToModelProfile : Profile
+    {
+        public DtoToModelProfile()
+        {
+            CreateMap<UserModel, UserDto>() //Criando o mapeamendo de Model para Dto
+            .ReverseMap(); // Revertendo o mapeamento de Dto para Model
+
+
+        }
+    }
+}
