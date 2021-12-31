110e45db11eb87b69a42795c069101e463d58501
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-12-31 16:45:38 -0300

    Implementando teste sobre o login de usuario

---
+++ src/Api.Service.Test/Login/QuandoForExecutadoFindByLogin.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.Users;
+using Moq;
+using Xunit;
+using Api.Domain.Dtos;
+
+namespace Api.Service.Test.Login
+{
+    public class QuandoForExecutadoFindByLogin
+    {
+        private ILoginService _service;
+
+        private Mock<ILoginService> _serviceMock;
+
+        [Fact(DisplayName = "É possivel executar o método FindByLogin")]
+        public async Task E_Possivel_Executar_Metodo_FindByLogin()
+        {
+            var email = Faker.Internet.Email();
+
+            var loginDto = new LoginDto
+            {
+                Email = email
+            };
+
+            var objetoRetorno = new
+            {
+                authenticated = true,
+                created = DateTime.Now,
+                expiration = DateTime.Now.AddHours(8),
+                acessToken = Guid.NewGuid(),
+                userName = email,
+                message = "Usuario Logado com sucesso"
+            };
+
+            _serviceMock = new Mock<ILoginService>(); //Crição do Objeto fortemente tipado que utiliza a interface ILoginService, sendo então fortemente tipado. 
+            _serviceMock.Setup(m => m.FindByLogin(loginDto)).ReturnsAsync(objetoRetorno); //Realizando o Setup do mock, dizendo que quando chamar o método FindByLogin passando um "loginDto" como parametro, o retorno será um "objetoRetorno"
+            _service = _serviceMock.Object;
+
+
+            var result = await _service.FindByLogin(loginDto);
+            Assert.NotNull(result);
+        }
+    }
+}
