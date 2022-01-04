e958be908c01b7a761d993e23191ab069beceae3
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2022-01-03 22:31:51 -0300

    Implementando Dtos

---
+++ src/Api.Domain/Dtos/Municipio/MunicipioDtoUpdate.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Dtos.Municipio
+{
+    public class MunicipioDtoUpdate
+    {
+        [Required(ErrorMessage = "Id é um campo obrigatório")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "É obrigratório informar o nome do municipio.")]
+        [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo {1} caracteres.")]
+        public string Nome { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Código do IBGE inválido.")]
+        public int CodIBGE { get; set; }
+
+        [Required(ErrorMessage = "Código UF é um campo obrigatório")]
+        public Guid UfId { get; set; }
+    }
+}
